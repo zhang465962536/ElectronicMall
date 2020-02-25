f4aa67f4b1b90f306bfa96712d3fd3859103df80
zhangxiaohei <1013984329@qq.com>
2020-02-25 16:24:15 +0800

    商品详情UI架构设计

--- my-ec/src/main/java/com/example/my/ec/detail/GoodsDetailDelegate.java
+++ my-ec/src/main/java/com/example/my/ec/detail/GoodsDetailDelegate.java
@@ -1,20 +1,98 @@
 package com.example.my.ec.detail;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
+import com.ToxicBakery.viewpager.transforms.DefaultTransformer;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.bigkoo.convenientbanner.ConvenientBanner;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.my.ec.R;
+import com.example.my.ec.R2;
 import com.example.my_core.delegates.LatteDelegate;
+import com.example.my_core.net.RestClient;
+import com.example.my_core.net.callback.ISuccess;
+import com.example.my_core.ui.banner.HolderCreator;
+import com.example.my_core.ui.widget.CircleTextView;
+import com.example.my_core.util.file.FileUtil;
+import com.example.my_core.util.log.ToastUtil;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.tabs.TabLayout;
+import com.joanzapata.iconify.widget.IconTextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 import me.yokeyword.fragmentation.anim.DefaultHorizontalAnimator;
 import me.yokeyword.fragmentation.anim.FragmentAnimator;
 
-public class GoodsDetailDelegate extends LatteDelegate {
+public class GoodsDetailDelegate extends LatteDelegate implements AppBarLayout.OnOffsetChangedListener {
 
-    public static GoodsDetailDelegate create(){
-        return new GoodsDetailDelegate();
+    @BindView(R2.id.goods_detail_toolbar)
+    Toolbar mToolbar = null;
+    @BindView(R2.id.tab_layout)
+    TabLayout mTabLayout = null;
+    @BindView(R2.id.view_pager)
+    ViewPager mViewPager = null;
+    @BindView(R2.id.detail_banner)
+    ConvenientBanner<String> mBanner = null;
+    @BindView(R2.id.collapsing_toolbar_detail)
+    CollapsingToolbarLayout mCollapsingToolbarLayout = null;
+    @BindView(R2.id.app_bar_detail)
+    AppBarLayout mAppBar = null;
+
+    //底部
+    @BindView(R2.id.icon_favor)
+    IconTextView mIconFavor = null;
+    @BindView(R2.id.tv_shopping_cart_amount)
+    CircleTextView mCircleTextView = null;
+    @BindView(R2.id.rl_add_shop_cart)
+    RelativeLayout mRlAddShopCart = null;
+    @BindView(R2.id.icon_shop_cart)
+    IconTextView mIconShopCart = null;
+
+    private static final String ARG_GOODS_ID = "ARG_GOODS_ID";
+    private int mGoodsId = -1;
+
+    private String mGoodsThumbUrl = null;
+    private int mShopCount = 0;
+
+    private static final RequestOptions OPTIONS = new RequestOptions()
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .centerCrop()
+            .dontAnimate()
+            .override(100, 100);
+
+    //传入商品id
+    public static GoodsDetailDelegate create(@NonNull int goodsId){
+        final Bundle args = new Bundle();
+        args.putInt(ARG_GOODS_ID,goodsId);
+        final GoodsDetailDelegate delegate = new GoodsDetailDelegate();
+        delegate.setArguments(args);
+        return delegate;
+    }
+
+    //从oncreate  去除 bundle 的 goodsId数值
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final Bundle args = getArguments();
+        if(args!= null){
+            mGoodsId = args.getInt(ARG_GOODS_ID);
+            ToastUtil.QuickToast("商品Id 为 " + mGoodsId);
+        }
     }
 
     @Override
@@ -24,12 +102,59 @@ public class GoodsDetailDelegate extends LatteDelegate {
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+        //伸缩变化的颜色
+        mCollapsingToolbarLayout.setContentScrimColor(Color.WHITE);
+        mAppBar.addOnOffsetChangedListener(this);
+        initNoNetData();
+    }
 
+    private void initNoNetData() {
+        String json = FileUtil.getRawFile(R.raw.goods_detail_data);
+        final JSONObject data = JSON.parseObject(json).getJSONObject("data");
+        initBanner(data);
+    }
+
+    //返回数据
+    private void initData(){
+        RestClient.builder()
+                .url("goods_detail.php")
+                .params("goods_id",mGoodsId)
+                .loader(getContext())
+                .success(new ISuccess() {
+                    @Override
+                    public void onSuccess(String response) {
+                        final JSONObject data = JSON.parseObject(response).getJSONObject("data");
+                        initBanner(data);
+                    }
+                })
+                .build()
+                .get();
+    }
+
+    private void initBanner(JSONObject data){
+        final JSONArray array = data.getJSONArray("banners");
+        final List<String> images = new ArrayList<>();
+        final int size = array.size();
+        for(int i = 0;i < size ; i ++){
+            images.add(array.getString(i));
+        }
+
+        mBanner.setPages(new HolderCreator(),images)
+                .setPageIndicator(new int[]{R.drawable.dot_normal,R.drawable.dot_focus})
+                .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.CENTER_HORIZONTAL)
+                .setPageTransformer(new DefaultTransformer())
+                .startTurning(3000)
+                .setCanLoop(true);
     }
 
     //跳转该页面时候 添加水平动画
     @Override
     public FragmentAnimator onCreateFragmentAnimator() {
         return new DefaultHorizontalAnimator();
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+
     }
 }
--- my-ec/src/main/java/com/example/my/ec/main/index/IndexItemClickListener.java
+++ my-ec/src/main/java/com/example/my/ec/main/index/IndexItemClickListener.java
@@ -6,6 +6,8 @@ import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.listener.SimpleClickListener;
 import com.example.my.ec.detail.GoodsDetailDelegate;
 import com.example.my_core.delegates.LatteDelegate;
+import com.example.my_core.ui.recycler.MultipleFields;
+import com.example.my_core.ui.recycler.MultipleItemEntity;
 
 public class IndexItemClickListener extends SimpleClickListener {
 
@@ -23,7 +25,9 @@ public class IndexItemClickListener extends SimpleClickListener {
 
     @Override
     public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
-        final GoodsDetailDelegate delegate = GoodsDetailDelegate.create();
+        final MultipleItemEntity entity = (MultipleItemEntity) baseQuickAdapter.getData().get(position);
+        final int goodsId = entity.getField(MultipleFields.ID);
+        final GoodsDetailDelegate delegate = GoodsDetailDelegate.create(goodsId);
         DELEGATE.start(delegate);
     }
 
